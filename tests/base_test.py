5d3a8ddcda491791e0046164daab93b1bcdba966
Vitaliy Filin <vitalyfilin88@gmail.com>
2023-05-06 02:06:35 +0400

    Fixed default path for base_test

--- tests/base_test.py
+++ tests/base_test.py
@@ -2,12 +2,14 @@ import unittest
 
 
 class BaseTest(unittest.TestCase):
-    def __init__(self, method_name='runTest', pdf_class=None):
+    default_path = r'D:\JetBrains\PycharmProjects\PdfTest\data\input\test_task.pdf'
+
+    def __init__(self, method_name='runTest', pdf_class=None, pdf_file=default_path):
         super().__init__(methodName=method_name)
         self.pdf_class = pdf_class
+        self.pdf_file = pdf_file
 
     def setUp(self):
-        self.pdf_file = r'D:\JetBrains\PycharmProjects\PdfTest\data\input\test_task.pdf'
         if self.pdf_class:
             self.pdf = self.pdf_class(self.pdf_file)
 
